cfa272834ab9082e08cfee279fed60b6ad5541a4
Darian <116446349+justDarian@users.noreply.github.com>
2024-01-02 19:21:57 -0500

    whoopsies

--- start.bat
+++ start.bat
@@ -12,16 +12,6 @@ if %errorlevel% neq 0 (
 title Checking libraries...
 python -m pip install -r requirements.txt
 
-cls
-echo Starting Bypass Mapper...
-start mapper.exe
-
-title hyperinjector is NOT stalled here
-echo Installing the modded ROBLOX client...
-timeout /nobreak 2
-echo Completed! Injecting into the service
-timeout /nobreak 15
-
 title Starting hyperinjector...
 python hyperinjector.py
 if %errorlevel% neq 0 goto ERROR
